53a4bd92a8bfac1c79bd5b4200824c6c2a4f2770
Figueirax <pedrofigueiredodias08@gmail.com>
2024-11-27 17:57:58 -0300

    Changes in utils and analysis

--- inc/utils.h
+++ inc/utils.h
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-double ** create_matrix(int N);
-double ** create_matrix_and_init(int N);
+double **create_matrix(int N);
+double **create_matrix_and_init(int N);
 void free_matrix(double **matrix, int N);
+void salvar_matriz(double **matriz, int linhas, int colunas, const char *nome_arquivo);
 
 #endif
