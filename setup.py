9c753bff28cf7961370030ec5720ed31a68f4d67
eduardoveri <verissimo.eduardo@unifesp.br>
2024-12-02 16:57:40 -0300

    [FEAT] Bump version to 2.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='diffusion',
-    version='1.0',
+    version='2.0',
     description='Python module for solving diffusion equations using a shared C library',
     py_modules=['diffusion'],
     install_requires=[
