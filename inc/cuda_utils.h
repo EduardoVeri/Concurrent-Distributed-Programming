af3f26f1d1ba617ea206bf55383af99ab179f362
eduardoveri <verissimo.eduardo@unifesp.br>
2024-12-28 17:10:05 -0300

    [FEAT] Declare get_number_of_threads function in cuda_utils.h

--- inc/cuda_utils.h
+++ inc/cuda_utils.h
@@ -11,6 +11,7 @@ void cuda_init(double *h_C_flat, double *h_C_new_flat, DiffEqArgs *args);
 void cuda_get_result(double *h_C_flat, int N);
 void cuda_finalize();
 void set_block_dimensions(int x, int y);
+int get_number_of_threads();
 
 #ifdef __cplusplus
 }
