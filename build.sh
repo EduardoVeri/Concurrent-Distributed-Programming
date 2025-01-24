b4f315e80b0ebc48f2f836acd9430a9aae496a4d
eduardoveri <verissimo.eduardo@unifesp.br>
2025-01-24 15:11:22 -0300

    [STYLE] Improve build script output formatting with color codes in variables for a more semantic code

--- build.sh
+++ build.sh
@@ -6,16 +6,21 @@ if [ ! -f "build.sh" ]; then
     exit 1
 fi
 
+RED="\e[1;31m"
+YELLOW="\e[1;33m"
+GREEN="\e[1;32m"
+RESET="\e[0m"
+
 NVCC=$(which nvcc)
 if [ -z "$NVCC" ]; then
-    echo -e "\e[1;33mWARNING: NVIDIA CUDA Compiler (nvcc) not found.\e[0m"
-    echo -e "\e[1;33mPlease install the NVIDIA CUDA Toolkit to build the CUDA version.\e[0m"
+    echo -e "${YELLOW}WARNING: NVIDIA CUDA Compiler (nvcc) not found.${RESET}"
+    echo -e "${YELLOW}Please install the NVIDIA CUDA Toolkit to build the CUDA version.${RESET}"
 fi
 
 MPI=$(which mpicc)
 if [ -z "$MPI" ]; then
-    echo -e "\e[1;33mWARNING: Message Passing Interface (MPI) Compiler (mpicc) not found.\e[0m"
-    echo -e "\e[1;33mPlease install the OpenMPI library to build the MPI version.\e[0m"
+    echo -e "${YELLOW}WARNING: Message Passing Interface (MPI) Compiler (mpicc) not found.${RESET}"
+    echo -e "${YELLOW}Please install the OpenMPI library to build the MPI version.${RESET}"
 fi
 
 # Create the build directory
@@ -24,26 +29,26 @@ cd build
 
 # Build the shared library
 gcc $(ls ../src/*.c | grep -v 'mpi.c') -I../inc -o libDiffusionEquation.so -fopenmp -shared -DBUILD_SHARED
-echo -e "\e[1;32mSuccessfully built libDiffusionEquation.so\e[0m"
-echo -e "\e[1;33mWARNING: The shared library does not support the MPI version yet.\e[0m"
+echo -e "${GREEN}Successfully built libDiffusionEquation.so${NC}"
+echo -e "${YELLOW}WARNING: The shared library does not support the MPI version yet.${RESET}"
 
 if [ ! -z "$NVCC" ]; then
     nvcc -Xcompiler -fPIC -shared -I../inc ../src/cuda.cu -o libCUDAdiffusionEquation.so -arch=sm_50
-    echo -e "\e[1;32mSuccessfully built libCUDAdiffusionEquation.so\e[0m"
+    echo -e "${GREEN}Successfully built libCUDAdiffusionEquation.so${RESET}"
 fi
 
 # Build the executables
 gcc ../src/sequential.c ../src/utils.c -I../inc -o sequential
-echo -e "\e[1;32mSuccessfully built sequential\e[0m"
+echo -e "${GREEN}Successfully built sequential${RESET}"
 gcc ../src/omp.c ../src/utils.c -I../inc -o omp -fopenmp
-echo -e "\e[1;32mSuccessfully built omp\e[0m"
+echo -e "${GREEN}Successfully built omp${RESET}"
 
 if [ ! -z "$NVCC" ]; then
     nvcc ../src/cuda.cu ../src/utils.c -I../inc -o cuda -arch=sm_50
-    echo -e "\e[1;32mSuccessfully built cuda\e[0m"
+    echo -e "${GREEN}Successfully built cuda${RESET}"
 fi
 
 if [ ! -z "$MPI" ]; then
     mpicc ../src/mpi.c ../src/utils.c -I../inc -o mpi -fopenmp
-    echo -e "\e[1;32mSuccessfully built mpi\e[0m"
+    echo -e "${GREEN}Successfully built mpi${RESET}"
 fi
