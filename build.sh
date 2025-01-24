a50c54d17f738836efee986a90f015217f3a38aa
eduardoveri <verissimo.eduardo@unifesp.br>
2025-01-24 15:05:30 -0300

    [FEATURE] Enhance build script to check for MPI compiler and provide warnings

--- build.sh
+++ build.sh
@@ -12,13 +12,20 @@ if [ -z "$NVCC" ]; then
     echo -e "\e[1;33mPlease install the NVIDIA CUDA Toolkit to build the CUDA version.\e[0m"
 fi
 
+MPI=$(which mpicc)
+if [ -z "$MPI" ]; then
+    echo -e "\e[1;33mWARNING: Message Passing Interface (MPI) Compiler (mpicc) not found.\e[0m"
+    echo -e "\e[1;33mPlease install the OpenMPI library to build the MPI version.\e[0m"
+fi
+
 # Create the build directory
 mkdir -p build
 cd build
 
 # Build the shared library
-gcc ../src/*.c -I../inc -o libDiffusionEquation.so -fopenmp -shared -DBUILD_SHARED
+gcc $(ls ../src/*.c | grep -v 'mpi.c') -I../inc -o libDiffusionEquation.so -fopenmp -shared -DBUILD_SHARED
 echo -e "\e[1;32mSuccessfully built libDiffusionEquation.so\e[0m"
+echo -e "\e[1;33mWARNING: The shared library does not support the MPI version yet.\e[0m"
 
 if [ ! -z "$NVCC" ]; then
     nvcc -Xcompiler -fPIC -shared -I../inc ../src/cuda.cu -o libCUDAdiffusionEquation.so -arch=sm_50
@@ -34,4 +41,9 @@ echo -e "\e[1;32mSuccessfully built omp\e[0m"
 if [ ! -z "$NVCC" ]; then
     nvcc ../src/cuda.cu ../src/utils.c -I../inc -o cuda -arch=sm_50
     echo -e "\e[1;32mSuccessfully built cuda\e[0m"
+fi
+
+if [ ! -z "$MPI" ]; then
+    mpicc ../src/mpi.c ../src/utils.c -I../inc -o mpi -fopenmp
+    echo -e "\e[1;32mSuccessfully built mpi\e[0m"
 fi
