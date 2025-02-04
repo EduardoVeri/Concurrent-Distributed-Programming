27861874448adbe1dfeead225218193f5da82d3b
eduardoveri <verissimo.eduardo@unifesp.br>
2025-02-04 19:00:04 -0300

    [FEATURE] Increase simulation parameters for improved performance and adjust output file naming

--- scripts/run_simulations.sh
+++ scripts/run_simulations.sh
@@ -14,12 +14,12 @@ rm -r results
 mkdir -p results
 cd results
 
-N=50
+N=2000
 N_EVAL=15
-N_STEPS=50
+N_STEPS=500
 
 echo -e "${YELLOW}Running simulations...${RESET}"
-../sequential $N_EVAL $N $N_STEPS 0.1 0.01 1.0 > seq.txt
+../sequential $N_EVAL $N $N_STEPS 0.1 0.01 1.0 >seq.txt
 echo -e "${GREEN}Sequential simulation completed${RESET}"
 
 # for i in 1 2 4 8 16 32; do
@@ -29,15 +29,15 @@ echo -e "${GREEN}Sequential simulation completed${RESET}"
 # done
 
 echo -e "${YELLOW}Running OpenMP simulation with 4 thread...${RESET}"
-../omp $N_EVAL $N $N_STEPS 0.1 0.01 1.0 4 > omp_4.txt
+../omp $N_EVAL $N $N_STEPS 0.1 0.01 1.0 4 >omp_4.txt
 echo -e "${GREEN}OpenMP simulation with 4 thread completed${RESET}"
 
 echo -e "${YELLOW}Running CUDA simulation...${RESET}"
-../cuda $N_EVAL $N $N_STEPS 0.1 0.01 1.0 > cuda.txt
+../cuda $N_EVAL $N $N_STEPS 0.1 0.01 1.0 >cuda.txt
 echo -e "${GREEN}CUDA simulation completed${RESET}"
 
 for i in 1 2 4 8 16 32; do
     echo -e "${YELLOW}Running MPI simulation with $i processes...${RESET}"
-    mpirun -np $i --oversubscribe ../mpi $N_EVAL $N $N_STEPS 0.1 0.01 1.0 1 > omp_$i.txt
+    mpirun -np $i --oversubscribe ../mpi $N_EVAL $N $N_STEPS 0.1 0.01 1.0 1 >mpi_$i.txt
     echo -e "${GREEN}MPI simulation with $i processes completed${RESET}"
 done
